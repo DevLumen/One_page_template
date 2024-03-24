b1ff2d40defe79c7fe85b8fcbfadb3f1b3abce1c
Andres S <devdre783@Andress-MBP.lan>
2024-03-23 18:34:14 -0700

    set up the home main page segmented into 4 sections, blocks 1 through 3 and the footer

--- main/src/App.js
+++ main/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Home from './components/Landing page/home';
 // import { Route, Routes } from "react-router-dom";
 import Navbar from './components/Navbar/navbar';
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <Home />
     </div>
   );
 }
---
+++ main/src/components/Landing page/home.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import '../styles/home.css';
+
+export default function Home() {
+    return (
+        <div className="main_container">
+            <div className="block_one">
+                {/* <h1>MAIN PAGE COMPONENT 1 GOES HERE</h1> */}
+            </div>
+            <div className="block_two">
+                {/* <h1>MAIN PAGE COMPONENT 2 GOES HERE</h1> */}
+            </div>
+            <div className="block_three">
+                {/* <h1>MAIN PAGE COMPONENT 3 GOES HERE</h1> */}
+            </div>
+            <div className="footer_block">
+                {/* <h1>FOOTER COMPONENT GOES HERE</h1> */}
+            </div>
+        </div>
+    )
+}
