2a02bee4566e8c01e0a0b649b55ce03fb9ca51a6
Andres S <devdre783@Andress-MBP.lan>
2024-04-26 16:23:47 -0700

    redesigned the splash page, WIP

--- main/src/components/Welcome page/welcome_page.js
+++ main/src/components/Welcome page/welcome_page.js
@@ -1,40 +1,55 @@
 import React, { useState, useEffect } from "react";
 import '../styles/welcomepage.css';
-import me from '../../images/My_Profile.png';
+import myAvatar from '../../images/My_Profile.png';
 import { ReactTyped } from 'react-typed'
 
 export default function WelcomePage() {
+
+    const [isShown, setIsShown] = useState(false);
 
     return (
         <div id="home" className="welcome_container">
             <div className="content_container">
                 <div className="header_container">
-                    <h1 className="header_text">Welcome.</h1>
+                    <h1 className={isShown ? 'fade-out' : "header_text"} onClick={() => setIsShown(true)}>Welcome.</h1>
                 </div>
-                <img className="my_pic" src={me}></img>
-              
-              {/* I am a... */}
-                <h1>
-                    <ReactTyped
-                        strings={["Frontend Developer ^2000", "QA Engineer ^2000", "UI/UX Designer ^2000"]}
-                        typeSpeed={55}
-                        loop
-                        backSpeed={35}
-                        cursorChar=">"
-                        showCursor={true}
-                    />
-                </h1>
-
-                {/* About me... */}
-                <h2>
-                    <ReactTyped strings={[
-                        "Welcome, my name is Andres Soca. ^2000",
-                        "I design and code beautiful projects, and I absolutely love what I do. ^4000"
-                    ]}
-                        typeSpeed={15}
-                        loop
-                    ></ReactTyped>
-                </h2>
+                {/* isShown being true will set this div and its content to visible if the welcome is clicked */}
+                <div className={!isShown ? 'notVisible' : "fade-in"}>
+                    <div className="content">
+                        <img className="my_pic" src={myAvatar} />
+                        <div className="typed_anim">
+                            {/* I am a... */}
+                            <h1>
+                                <ReactTyped
+                                    strings={[
+                                        "Frontend Developer ^2000",
+                                        "QA Engineer ^2000",
+                                        "UI/UX Designer ^2000"
+                                    ]}
+                                    typeSpeed={55}
+                                    loop
+                                    backSpeed={35}
+                                    cursorChar=">"
+                                    showCursor={true}
+                                />
+                            </h1>
+                        </div>
+                        <div className="speechBubble_container">
+                            <div className="speechBubble_typedAnim">
+                                {/* About me... */}
+                                <h2>
+                                    <ReactTyped strings={[
+                                        "Welcome, my name is Andres Soca. ^2000",
+                                        "I design and code beautiful projects, and I absolutely love what I do. ^4000"
+                                    ]}
+                                        typeSpeed={15}
+                                        loop
+                                    ></ReactTyped>
+                                </h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
