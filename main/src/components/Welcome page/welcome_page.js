64017063b4b2d4f7b64e8634a598b63f444118ef
Andres S <devdre783@Andress-MacBook-Pro.local>
2024-04-29 17:55:06 -0700

    added a condiitonal rendering on the home page, elements only load once 'Welcome' is clicked

--- main/src/components/Welcome page/welcome_page.js
+++ main/src/components/Welcome page/welcome_page.js
@@ -6,18 +6,19 @@ import { ReactTyped } from 'react-typed'
 export default function WelcomePage() {
 
     const [isShown, setIsShown] = useState(false);
+    const [loaded, setLoaded] = useState(false);
 
     return (
         <div id="home" className="welcome_container">
             <div className="content_container">
                 <div className="header_container">
-                    <h1 className={isShown ? 'fade-out' : "header_text"} onClick={() => setIsShown(true)}></h1>
-                    <h1 className={isShown ? 'fade-out' : "header_text"} onClick={() => setIsShown(true)} data-text="Welcome.">Welcome.</h1>
+                    <h1 className={isShown ? 'fade-out' : "header_text"} onClick={() => {setIsShown(true); setLoaded(true);}} data-text="Welcome.">Welcome.</h1>
                 </div>
                 {/* isShown being true will set this div and its content to visible if the welcome is clicked */}
                 <div className={!isShown ? 'notVisible' : "fade-in"}>
                     <div className="content">
                         <img className="my_pic" src={myAvatar} />
+                    
                         <div className="typed_anim">
                             {/* I am a... */}
                             <h1>
@@ -35,31 +36,35 @@ export default function WelcomePage() {
                                 />
                             </h1>
                         </div>
-                        <div className="speechBubble_container1">
-                            <div className="speechBubble_typedAnim1">
-                                <h2 className="speech_text1">
-                                    {/* About me... */}
-                                    <ReactTyped strings={[
-                                        "Welcome, my name is Andres Soca. ^2000"
-                                    ]}
-                                        typeSpeed={15}
-                                        loop
-                                    ></ReactTyped>
-                                </h2>
+
+                        {loaded ? 
+                            <div className="speechBubble_container1">
+                                <div className="speechBubble_typedAnim1">
+                                    <h2 className="speech_text1">
+                                        {/* About me... */}
+                                        <ReactTyped strings={[
+                                            "^550 Welcome, my name is Andres Soca."
+                                        ]}
+                                            typeSpeed={20}
+                                        ></ReactTyped>
+                                    </h2>
+                                </div>
                             </div>
-                        </div>
+                        : null}
+
+                        {loaded ?
                         <div className="speechBubble_container2">
                             <div className="speechBubble_typedAnim2">
                                 <h2 className="speech_text2">
                                     <ReactTyped strings={[
-                                        "I design and code beautiful projects, and I absolutely love what I do. ^4000"
+                                        "^550 I design and code beautiful projects, and I absolutely love what I do."
                                     ]}
-                                        typeSpeed={15}
-                                        loop
+                                        typeSpeed={25}
                                     ></ReactTyped>
                                 </h2>
                             </div>
                         </div>
+                        : null}
                     </div>
                 </div>
             </div>
